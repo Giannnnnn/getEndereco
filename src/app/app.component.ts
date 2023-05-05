99d3baabb86aa624ef4f1788fc22fa4bacbb5e00
Giovani Florek <giremio5@gmail.com>
2023-05-05 17:56:37 -0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'getEndereco';
+  public cep: string = '';
+  public address: any;
+  public showData: boolean = false;
+  public formGroup: FormGroup = new FormGroup([]);
+
+  constructor(private http: HttpClient, private _snackBar: MatSnackBar) {
+    this.formGroup.addControl('cep', new FormControl(''));
+    this.formGroup.controls['cep'].valueChanges.subscribe((value) => {
+      this.formIsInvalid();
+    });
+  }
+
+  onSubmit() {
+    this.showData = false;
+    this.http.get(`https://viacep.com.br/ws/${this.cep}/json/`).subscribe((data: any) => {
+      if (data.erro) {
+        this._snackBar.open('Não encontrado', 'x');
+        setTimeout(() => {
+          this._snackBar.dismiss();
+        }, 2000);
+      } else {
+        this.address = data;
+        this.showData = true;
+        this._snackBar.open('Endereço encontrado!', 'x');
+        setTimeout(() => {
+          this._snackBar.dismiss();
+        }, 2000);
+      }
+
+    }, (error) => {
+      this._snackBar.open(error.message, 'x');
+      setTimeout(() => {
+        this._snackBar.dismiss();
+      }, 2000);
+    });
+  }
+
+  public formIsInvalid(): boolean {
+    if (this.cep.length < 8) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  public cleanSearch() {
+    this.showData = false;
+    this.address = null;
+    this.cep = '';
+    this.formGroup.controls['cep'].setValue('');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatCardModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatButtonToggleModule,
+    MatTableModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
     BrowserModule,
-    AppRoutingModule
+    MatSlideToggleModule,
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [MatSnackBar],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
